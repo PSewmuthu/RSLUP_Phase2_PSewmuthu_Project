3cc1d1d1c523867b726d98883a20ffad26e88cab
PSewmuthu <apasindusewmuthu@gmail.com>
2023-11-29 15:49:45 +0530

    Create order model

---
+++ PokemonApp/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using PokemonReview.Models;
+
+namespace PokemonApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Pokemon Pokemon { get; set; }
+    }
+}
