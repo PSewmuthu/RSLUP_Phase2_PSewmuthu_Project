5be78a8c96a9340ce41fc28fd1f15c2a4b3ee322
PSewmuthu <apasindusewmuthu@gmail.com>
2023-11-29 13:59:52 +0530

    Create Pokemon model

---
+++ PokemonApp/Models/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace PokemonReview.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
