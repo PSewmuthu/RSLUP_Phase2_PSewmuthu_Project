2fef7db4c250cb590616db5c28d9322070f77ddc
PSewmuthu <apasindusewmuthu@gmail.com>
2023-11-29 21:38:50 +0530

    Create repository for User

---
+++ PokemonApp/Repositories/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using PokemonApp.Data;
+using PokemonApp.Interfaces;
+using PokemonApp.Models;
+
+namespace PokemonApp.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+        public UserRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public bool CreateUser(User user)
+        {
+            _context.Add(user);
+            return Save();
+        }
+
+        public bool DeleteUser(User user)
+        {
+            _context.Remove(user);
+            return Save();
+        }
+
+        public User GetUser(int id)
+        {
+            return _context.Users.Where(u => u.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<User> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateUser(User user)
+        {
+            _context.Update(user);
+            return Save();
+        }
+
+        public bool UserExists(int userId)
+        {
+            return _context.Users.Any(u => u.Id == userId);
+        }
+    }
+}
