f441fd33a68ecbcc18e8c36f297ed7d3a333a4e9
PSewmuthu <apasindusewmuthu@gmail.com>
2023-11-29 19:32:22 +0530

    Test run

---
+++ PokemonApp/Migrations/20231129130225_MigrateAfterFixMistakes.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonApp.Migrations
+{
+    public partial class MigrateAfterFixMistakes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_Pokemon_PokemonId",
+                table: "Order");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Order",
+                table: "Order");
+
+            migrationBuilder.RenameTable(
+                name: "Order",
+                newName: "Orders");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Order_PokemonId",
+                table: "Orders",
+                newName: "IX_Orders_PokemonId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Pokemon_PokemonId",
+                table: "Orders",
+                column: "PokemonId",
+                principalTable: "Pokemon",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Pokemon_PokemonId",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "Order");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_PokemonId",
+                table: "Order",
+                newName: "IX_Order_PokemonId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Order",
+                table: "Order",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_Pokemon_PokemonId",
+                table: "Order",
+                column: "PokemonId",
+                principalTable: "Pokemon",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
