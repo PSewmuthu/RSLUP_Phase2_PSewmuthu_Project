456891a39c6c89be67bc1109961ff3a5f2114f6a
PSewmuthu <apasindusewmuthu@gmail.com>
2023-11-29 16:16:04 +0530

    Create interface for order

---
+++ PokemonApp/Interfaces/IOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using PokemonApp.Models;
+using PokemonReview.Models;
+
+namespace PokemonApp.Interfaces
+{
+    public interface IOrderRepository
+    {
+        ICollection<Order> GetOrders();
+        Order GetOrder(int orderId);
+        ICollection<Order> GetOrderOfAPokemon(int pokeId);
+        ICollection<Pokemon> GetPokemonByOrder(int ownerId);
+        bool OrderExists(int orderId);
+        bool CreateOrder(Order order);
+        bool UpdateOrder(Order order);
+        bool DeleteOrder(Order order);
+        bool Save();
+    }
+}
