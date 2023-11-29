fb5a587cbe028a164da65fcdf84798f439c458d6
PSewmuthu <apasindusewmuthu@gmail.com>
2023-11-29 16:29:07 +0530

    Create repository for order

---
+++ PokemonApp/Repositories/OrderRepository.cs
@@ -0,0 +1,55 @@
+﻿using PokemonApp.Interfaces;
+using PokemonApp.Models;
+using PokemonReview.Data;
+using PokemonReview.Models;
+
+namespace PokemonApp.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly DataContext _context;
+        public OrderRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool CreateOrder(Order order)
+        {
+            _context.Add(order);
+            return Save();
+        }
+
+        public bool DeleteOrder(Order order)
+        {
+            _context.Remove(order);
+            return Save();
+        }
+
+        public Order GetOrder(int orderId)
+        {
+            return _context.Orders.Where(o => o.Id == orderId).FirstOrDefault();
+        }
+
+        public ICollection<Order> GetOrders()
+        {
+            return _context.Orders.ToList();
+        }
+
+        public bool OrderExists(int orderId)
+        {
+            return _context.Orders.Any(o => o.Id == orderId);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateOrder(Order order)
+        {
+            _context.Update(order);
+            return Save();
+        }
+    }
+}
