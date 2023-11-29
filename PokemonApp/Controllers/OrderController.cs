bb6e2a2f6f0282f53b9e74b849130c7dbf7c230e
PSewmuthu <apasindusewmuthu@gmail.com>
2023-11-29 17:01:32 +0530

    Add controller for order

---
+++ PokemonApp/Controllers/OrderController.cs
@@ -0,0 +1,130 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonApp.Dtos;
+using PokemonApp.Interfaces;
+using PokemonApp.Models;
+using PokemonReview.Dto;
+using PokemonReview.Interfaces;
+
+namespace PokemonApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IMapper _mapper;
+        public OrderController(IOrderRepository orderRepository, IPokemonRepository pokemonRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _pokemonRepository = pokemonRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Order>))]
+        public IActionResult GetOrders()
+        {
+            var orders = _mapper.Map<List<OrderDto>>(_orderRepository.GetOrders());
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(orders);
+        }
+
+        [HttpGet("{orderId}")]
+        [ProducesResponseType(200, Type = typeof(Order))]
+        [ProducesResponseType(400)]
+        public IActionResult GetOrder(int orderId)
+        {
+            if (!_orderRepository.OrderExists(orderId))
+                return NotFound();
+
+            var order = _mapper.Map<OrderDto>(_orderRepository.GetOrder(orderId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(order);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateOrder([FromQuery] int pokemonId, [FromBody] OrderDto orderCreate)
+        {
+            if (orderCreate == null)
+                return BadRequest(ModelState);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var orderMap = _mapper.Map<Order>(orderCreate);
+
+            orderMap.Pokemon = _pokemonRepository.GetPokemon(pokemonId);
+
+            if (!_orderRepository.CreateOrder(orderMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while saving");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok("Successfully created");
+        }
+
+        [HttpPut("{orderId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult UpdateOrder(int orderId, [FromBody] OwnerDto updatedOrder)
+        {
+            if (updatedOrder == null)
+                return BadRequest(ModelState);
+
+            if (orderId != updatedOrder.Id)
+                return BadRequest(ModelState);
+
+            if (!_orderRepository.OrderExists(orderId))
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var orderMap = _mapper.Map<Order>(updatedOrder);
+
+            if (!_orderRepository.UpdateOrder(orderMap))
+            {
+                ModelState.AddModelError("", "Something went wrong updating order");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{orderId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult DeleteOrder(int orderId)
+        {
+            if (!_orderRepository.OrderExists(orderId))
+            {
+                return NotFound();
+            }
+
+            var orderToDelete = _orderRepository.GetOrder(orderId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_orderRepository.DeleteOrder(orderToDelete))
+            {
+                ModelState.AddModelError("", "Something went wrong deleting order");
+            }
+
+            return NoContent();
+        }
+    }
+}
