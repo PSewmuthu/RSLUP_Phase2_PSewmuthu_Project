bb3649f1aab92970189fb9e9ff302df284baed3b
PSewmuthu <apasindusewmuthu@gmail.com>
2023-11-30 05:38:56 +0530

    Add test cases

--- PokemonApp.Test/UnitTest1.cs
+++ PokemonApp.Test/UnitTest1.cs
@@ -1,11 +1,25 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
 namespace PokemonApp.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class ApiTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private HttpClient _httpClient;
+
+        public ApiTests()
         {
+            var webAppFactory = new WebApplicationFactory<Program>();
+            _httpClient = webAppFactory.CreateDefaultClient();
+        }
+
+        [TestMethod]
+        public async Task GetUserById()
+        {
+            var response = await _httpClient.GetAsync("");
+            var stringResult = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual("Test User", stringResult );
         }
     }
 }
